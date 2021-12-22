5c98c6bb576bbcddec9706d6b5d1fa7e31eb6256
yuwen <hiter1997@163.com>
2021-12-22 22:25:02 +0800

    first commit

---
+++ src/app1/app1.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+
+const $button1 = $("#add1");
+const $button2 = $("#minus1");
+const $button3 = $("#mul2");
+const $button4 = $("#divide2");
+const $number = $("#number");
+const n = localStorage.getItem("app1:n");
+$number.text(n || 100);
+
+$button1.on("click", () => {
+  let n = parseFloat($number.text());
+  n += 1;
+  localStorage.setItem("app1:n", n);
+  $number.text(n);
+});
+$button2.on("click", () => {
+  let n = parseFloat($number.text());
+  n -= 1;
+  localStorage.setItem("app1:n", n);
+  $number.text(n);
+});
+$button3.on("click", () => {
+  let n = parseFloat($number.text());
+  n *= 2;
+  localStorage.setItem("app1:n", n);
+  $number.text(n);
+});
+$button4.on("click", () => {
+  let n = parseFloat($number.text());
+  n /= 2;
+  localStorage.setItem("app1:n", n);
+  $number.text(n);
+});
